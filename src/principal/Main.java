b86ad3f74c7d865b50614043586ecff5cd091bef
Federico 'Magus' Rocca <frocca17@gmail.com>
2020-09-26 14:06:33 -0300

    Se agrega el paquete principal y se crea la primera ventana

---
+++ src/principal/Main.java
@@ -0,0 +1,13 @@
+package principal;
+
+public class Main
+{
+    
+    public static void main(String[] args)
+    {
+        Ventana mainWindow = new Ventana();
+        mainWindow.setLocationRelativeTo(null);
+        mainWindow.setVisible(true);
+    }
+    
+}
