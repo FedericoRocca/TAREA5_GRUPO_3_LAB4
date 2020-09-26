626b5b41405b1ba54d3f57df5095f8827b99d1d3
Federico 'Magus' Rocca <frocca17@gmail.com>
2020-09-26 16:09:45 -0300

    Validaciones de campos

--- src/paneles/AgregarPersona.java
+++ src/paneles/AgregarPersona.java
@@ -7,15 +7,98 @@ import javax.swing.JTree;
 import javax.swing.border.TitledBorder;
 import javax.swing.JButton;
 import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class AgregarPersona extends JPanel
 {
+    private JTextField txfNombre;
+    private JTextField txfApellido;
+    private JTextField txfDNI;
     
     public AgregarPersona()
     {
         setBackground(Color.LIGHT_GRAY);
         setLayout(null);
-        setBounds(0, 0, 500, 400);
+        setBounds(0, 0, 332, 115);
+        
+        JLabel lblNombre = new JLabel("Nombre");
+        lblNombre.setBounds(10, 11, 46, 14);
+        add(lblNombre);
+        
+        JLabel lblApellido = new JLabel("Apellido");
+        lblApellido.setBounds(10, 36, 46, 14);
+        add(lblApellido);
+        
+        JLabel lblDni = new JLabel("DNI");
+        lblDni.setBounds(10, 61, 46, 14);
+        add(lblDni);
+        
+        JButton btnAceptar = new JButton("Aceptar");
+        btnAceptar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // Se quiere guardar la persona, validamos datos
+                if( txfNombre.getText().equals("") )
+                {
+                    JOptionPane.showMessageDialog(null, "Debés ingresar un nombre", "Faltó el nombre!", JOptionPane.OK_OPTION);
+                } 
+                else if( txfApellido.getText().equals("") )
+                {
+                    JOptionPane.showMessageDialog(null, "Debés ingresar un apellido", "Faltó el apellido!", JOptionPane.OK_OPTION);
+                } 
+                else if( txfDNI.getText().equals("") )
+                {
+                    JOptionPane.showMessageDialog(null, "Debés ingresar un DNI", "Faltó el DNI!", JOptionPane.OK_OPTION);
+                }
+                
+                // Validamos largos
+                if( txfNombre.getText().length() > 45 )
+                {
+                    JOptionPane.showMessageDialog(null, "El nombre ingresado es demasiado largo", "Nombre inválido", JOptionPane.OK_OPTION);
+                }
+                else if( txfApellido.getText().length() > 45 )
+                {
+                    JOptionPane.showMessageDialog(null, "El apellido ingresado es demasiado largo", "Apellido inválido", JOptionPane.OK_OPTION);
+                }
+                
+                int dniIngresado = 0;
+                try
+                {
+                    Integer.parseInt(txfDNI.getText());
+                    dniIngresado = Integer.parseInt(txfDNI.getText());
+                }
+                catch (NumberFormatException ex)
+                {
+                    JOptionPane.showMessageDialog(null, "Debés ingresar un DNI válido", "DNI incorrecto!", JOptionPane.OK_OPTION);
+                }
+                
+                if( dniIngresado <= 0 )
+                {
+                    JOptionPane.showMessageDialog(null, "El DNI ingresado es inválido", "Apellido inválido", JOptionPane.OK_OPTION);
+                }
+                
+                // Llegamos hasta acá, las validaciones resultaron todas OK, guardamos en la base
+            }
+        });
+        btnAceptar.setBounds(10, 86, 89, 23);
+        add(btnAceptar);
+        
+        txfNombre = new JTextField();
+        txfNombre.setBounds(66, 8, 260, 20);
+        add(txfNombre);
+        txfNombre.setColumns(10);
+        
+        txfApellido = new JTextField();
+        txfApellido.setBounds(66, 33, 260, 20);
+        add(txfApellido);
+        txfApellido.setColumns(10);
+        
+        txfDNI = new JTextField();
+        txfDNI.setBounds(66, 58, 260, 20);
+        add(txfDNI);
+        txfDNI.setColumns(10);
     }
-    
 }
